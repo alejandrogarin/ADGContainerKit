218ba2dde7ccc7455f1f2708edcf87ee6ac0185e
Alejandro Garin <agarin@gmail.com>
2015-08-18 17:06:27 -0300

    refactor and minor fixes.

--- Pod/Classes/ContainerViewController.swift
+++ Pod/Classes/ContainerViewController.swift
@@ -25,15 +25,15 @@
 
 import UIKit
 
-public protocol ContainerViewControllerDelegate {
+public protocol ContainerViewControllerDelegate: class {
     func containerViewControllerWillStartTransition(controller: ContainerViewController)
     func containerViewControllerDidStartTransition(controller: ContainerViewController)
-    func containerViewController(controller: ContainerViewController, prepareForSegue segue: UIStoryboardSegue, sender: AnyObject?)
+    func containerViewController(controller: ContainerViewController, prepareViewController destinationViewController: UIViewController, sender: AnyObject?)
 }
 
 public class ContainerViewController: UIViewController {
     
-    public var delegate: ContainerViewControllerDelegate?
+    public weak var delegate: ContainerViewControllerDelegate?
     public var transitionDuration: Double = 0.5
     public var animationOption = UIViewAnimationOptions.TransitionFlipFromLeft
     public var currentSegueIdentifier: String?
@@ -43,9 +43,7 @@ public class ContainerViewController: UIViewController {
     // MARK: - Public API
         
     override public func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        
-        self.delegate?.containerViewController(self, prepareForSegue: segue, sender: sender)
-        
+        self.delegate?.containerViewController(self, prepareViewController: segue.destinationViewController, sender: sender)
         if let fromViewController = self.childViewControllers.first {
             self.swap(fromViewController: fromViewController, toViewController: segue.destinationViewController)
         } else {
@@ -62,21 +60,28 @@ public class ContainerViewController: UIViewController {
         self.delegate?.containerViewControllerWillStartTransition(self)
         self.performSegueWithIdentifier(identifier, sender: sender)
     }
+
+    public func emptyContainer() {
+        self.currentSegueIdentifier = nil
+        guard let childController = self.childViewControllers.first else {
+            return
+        }
+        childController.willMoveToParentViewController(nil)
+        childController.view.removeFromSuperview()
+        childController.removeFromParentViewController()
+    }
     
     // MARK: - Private
     
     private func constraintAddTopLeftBottomRight(fromView view:UIView, toSuperview superview: UIView) {
-        
         let top = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: superview, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0)
         let left = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: superview, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0)
         let right = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: superview, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0)
         let bottom = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: superview, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0)
-        
         superview.addConstraints([top, left, right, bottom])
     }
     
     private func addChild(viewController viewController: UIViewController) {
-        
         self.addChildViewController(viewController)
         let destView:UIView = viewController.view
         destView.translatesAutoresizingMaskIntoConstraints = false
@@ -88,22 +93,19 @@ public class ContainerViewController: UIViewController {
     }
     
     private func swap(fromViewController fromViewController: UIViewController, toViewController: UIViewController) {
-        
         toViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        fromViewController.willMoveToParentViewController(nil)
         self.addChildViewController(toViewController)
         
         self.transitionFromViewController(fromViewController, toViewController: toViewController, duration: self.transitionDuration, options: self.animationOption, animations: { () -> Void in
-            
             fromViewController.view.removeFromSuperview()
             self.view.addSubview(toViewController.view)
             self.constraintAddTopLeftBottomRight(fromView: toViewController.view, toSuperview: self.view)
-            
-            }) { (finished: Bool) -> Void in
-            
-                fromViewController.removeFromParentViewController()
-                toViewController.didMoveToParentViewController(self)
-                self.transitionInProgress = false
-                self.delegate?.containerViewControllerDidStartTransition(self)
+        }) { (finished: Bool) -> Void in
+            fromViewController.removeFromParentViewController()
+            toViewController.didMoveToParentViewController(self)
+            self.transitionInProgress = false
+            self.delegate?.containerViewControllerDidStartTransition(self)
         }
     }
 }
